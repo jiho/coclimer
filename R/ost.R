96d5f06cbaf665e18a1cbbb5b40da619b6399aff
Jean-Olivier Irisson <irisson@normalesup.org>
2018-11-07 22:00:02 +0100

    Fix unit of O ovata benthic concentration

--- R/ost.R
+++ R/ost.R
@@ -3,7 +3,7 @@
 #' @format A tibble (data.frame like) with 574 rows and 13 columns
 #' \describe{
 #'   \item{date}{Date of observation.}
-#'   \item{benthic}{Benthic abundance in grams of *O. ovata* per gram of wet weight.}
+#'   \item{benthic}{Benthic abundance of *O. ovata* in number of cells per gram of wet weight.}
 #'   \item{planktonic}{Planktonic concentration of *O. ovata* in nb of cells per L.}
 #'   \item{chla, ..., temperature}{Ten environmental variables measured at the dates of observation.}
 #' }
