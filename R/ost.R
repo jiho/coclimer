1cb9cfe9fdfed93c3358e2a2d46623c548f243eb
Jean-Olivier Irisson <irisson@normalesup.org>
2018-11-07 11:10:49 +0100

    Add a data set to showcase the functions

---
+++ R/ost.R
@@ -0,0 +1,11 @@
+#' Time series of Ostreopsis ovata concentrations
+#'
+#' @format A tibble (data.frame like) with 574 rows and 3 columns
+#' \describe{
+#'   \item{date} Date of observation
+#'   \item{benthic} Benthic abundance in grams of Ostreopsis per gram of wet weight
+#'   \item{planktonic} Planktonic concentration in nb of cells per L.
+#' }
+#'
+#' @export
+"ost"
